c0ae4507229e881fd6c3e4e7abed9896228d8c4a
Hajime Hoshi <hajimehoshi@gmail.com>
2018-04-04 01:32:51 +0900

    graphics: Remove unused returning value from (*Image).ReplacePixels

--- image.go
+++ image.go
@@ -255,15 +255,12 @@ func (i *Image) Dispose() {
 // When len(p) is not appropriate, ReplacePixels panics.
 //
 // When the image is disposed, ReplacePixels does nothing.
-//
-// ReplacePixels always returns nil as of 1.5.0-alpha.
-func (i *Image) ReplacePixels(p []byte) error {
+func (i *Image) ReplacePixels(p []byte) {
 	i.copyCheck()
 	if i.isDisposed() {
-		return nil
+		return
 	}
 	i.shareableImage.ReplacePixels(p)
-	return nil
 }
 
 // A DrawImageOptions represents options to render an image on an image.
@@ -371,7 +368,7 @@ func NewImageFromImage(source image.Image, filter Filter) (*Image, error) {
 	i.addr = i
 	runtime.SetFinalizer(i, (*Image).Dispose)
 
-	_ = i.ReplacePixels(graphicsutil.CopyImage(source))
+	i.ReplacePixels(graphicsutil.CopyImage(source))
 	return i, nil
 }
 
